2a3a4809897dfa73e27ae4e74c32d870120450aa
Alkesst <alec.augal@gmail.com>
2018-12-04 10:19:40 +0100

    Fixed some errors with admin site

--- DjanGIUnchained/urls.py
+++ DjanGIUnchained/urls.py
@@ -1,4 +1,8 @@
 from django.conf.urls import url, include
 
-urlpatterns = {
-}
+
+# thanks to
+# https://stackoverflow.com/questions/43304923/typeerror-at-admin-set-object-is-not-reversible-and-argument-to-reverse-m?noredirect=1&lq=1
+urlpatterns = [
+
+]
