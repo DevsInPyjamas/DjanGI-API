6ac87fad20e212e61648a62a9e2615dd331acefd
Alkesst <alec.augal@gmail.com>
2018-12-05 12:52:44 +0100

    finished API rest for GET methods that returns serialized objects

--- DjanGIUnchained/urls.py
+++ DjanGIUnchained/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url, include
-
+from DjanGIUnchained import views
 
 # thanks to
 # https://stackoverflow.com/questions/43304923/typeerror-at-admin-set-object-is-not-reversible-and-argument-to-reverse-m?noredirect=1&lq=1
 urlpatterns = [
-
+    url(r'^all_types$', views.all_piece_types, name='Type pieces'),
+    url(r'^pieces_from_type$', views.all_pieces_from_this_type, name='All Type Pieces'),
+    url(r'^piece$', views.piece_data, name='All Type Pieces')
 ]
--- DjanGIUnchained/views.py
+++ DjanGIUnchained/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseBadRequest
 
+from DjanGIUnchained import models
+from django.core import serializers
 # Create your views here.
+
+
+def all_piece_types(request):
+    """
+        The mighty power of the request that requests all the piece types
+    :param request: literally the request
+    :return: all objects serialized as a JSON with a HTTP response
+    """
+    query_repsonse = models.PieceType.objects.all()
+    return HttpResponse(serializers.serialize('json', query_repsonse))
+
+
+def all_pieces_from_this_type(request):
+    """
+        The mighty power of the request that requests all the piece types
+    :param request: literally the request
+    :return: all objects serialized as a JSON with a HTTP response
+    """
+    piece_type_pk = None
+    if request.method == 'GET' and 'id' in request.GET:
+        piece_type_pk = request.GET['id']
+    if piece_type_pk is not None:
+        query_repsonse = models.Pieces.objects.filter(type_id=piece_type_pk)
+        return HttpResponse(serializers.serialize('json', query_repsonse))
+    else:
+        error_str = '<h1> BAD REQUEST </h1> <p>It is required to receive an id as argument as follows ' \
+                    '\"127.0.0.1:8000/pieces_from_type?id=[a-zA-Z]+\"</p>'
+        return HttpResponseBadRequest(error_str)
+
+
+def piece_data(request):
+    """
+        The mighty power of the request that requests all the piece types
+    :param request: literally the request
+    :return: all objects serialized as a JSON with a HTTP response
+    """
+    piece_id = None
+    if request.method == 'GET' and 'id' in request.GET:
+        piece_id = request.GET['id']
+    if piece_id is not None:
+        query_repsonse = models.Pieces.objects.filter(id=piece_id)
+        return HttpResponse(serializers.serialize('json', query_repsonse))
+    else:
+        error_str = '<h1> BAD REQUEST </h1> <p>It is required to receive an id as argument as follows ' \
+                    '\"127.0.0.1:8000/pieces_from_type?id=[a-zA-Z]+\"</p>'
+        return HttpResponseBadRequest(error_str)
